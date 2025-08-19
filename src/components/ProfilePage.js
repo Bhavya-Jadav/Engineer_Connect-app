5abca65305da90308a8994e4aebd2c6cca82a4cc
Bhavya-Jadav <jadavbhavya991599@gmail.com>
2025-08-19 16:56:19 +0530

    hell

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import { API_BASE_URL } from '../utils/api';
 import { useNavigate } from 'react-router-dom';
+import '../styles/auth.css';
 
 const ProfilePage = ({ 
   currentUser, 
